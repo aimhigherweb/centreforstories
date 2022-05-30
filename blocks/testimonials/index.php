d6aaa03ea430235418ca6af0a737471a3611a3d2
Amy Kapernick <amy@kapers.dev>
2022-05-31 06:50:14 +0800

    CU-22wwdgr[complete] testimonials block

--- blocks/testimonials/index.php
+++ blocks/testimonials/index.php
@@ -26,5 +26,20 @@
 <div 
 	class="<?php echo classes($block_classes); ?> wp-block" 
 >
-	<h2 class="<?php echo classes([$styles['heading']]); ?>"></h2>
+	<h2 class="<?php echo classes([$styles['heading']]); ?>">
+		<?php echo get_field('heading'); ?>
+	</h2>
+	<?php if( have_rows('testimonials') ): ?>
+		<ul>
+		<?php while( have_rows('testimonials') ): the_row(); ?>
+			<li class="<?php echo classes([$styles['testimonial']]); ?>">
+				<?php echo inline_svg(get_template_directory_uri() . '/src/img/quotes.svg'); ?>
+				<blockquote>
+					<?php echo get_sub_field('testimonial'); ?>
+					<cite>- <?php the_sub_field('person'); ?></cite>
+				</blockquote>
+			</li>
+		<?php endwhile; ?>
+		</ul>
+	<?php endif; ?>
 </div>
