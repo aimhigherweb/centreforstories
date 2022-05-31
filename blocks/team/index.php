f731efa42b99dfbf0a4505d124b7486f43e3b12e
Amy Kapernick <amy@kapers.dev>
2022-05-31 09:40:38 +0800

    CU-22wwdgx team block

--- blocks/team/index.php
+++ blocks/team/index.php
@@ -26,5 +26,28 @@
 <div 
 	class="<?php echo classes($block_classes); ?> wp-block" 
 >
-	<h2 class="<?php echo classes([$styles['heading']]); ?>"></h2>
+	<h2 class="<?php echo classes([$styles['heading']]); ?>">
+		<?php echo get_field('heading'); ?>
+	</h2>
+	<?php if( have_rows('profiles') ): ?>
+		<ul>
+		<?php while( have_rows('profiles') ): the_row();
+			$image = get_sub_field('image');
+		?>
+			<li class="<?php echo classes([$styles['profile']]); ?>">
+				<div class="<?php echo classes([$styles['image']]); ?>">
+					<?php if(check_array_field($image, 'sizes')): ?>
+						<img
+							alt="<?php echo $image['alt'] || ''; ?>"
+							src="<?php echo $image['sizes']['event_feed_block']; ?>"
+						/>
+					<?php endif; ?>
+				</div>
+				<h3><?php the_sub_field('name'); ?></h3>
+				<p class="<?php echo classes([$styles['role']]); ?>"><?php the_sub_field('role'); ?></p>
+				<p><?php the_sub_field('bio'); ?></p>
+			</li>
+		<?php endwhile; ?>
+		</ul>
+	<?php endif; ?>
 </div>
