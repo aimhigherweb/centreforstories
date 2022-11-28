018052862b125759bea2e0d687d4ce707bca0dd5
Amy Kapernick <amy@kapers.dev>
2022-11-28 12:31:11 +0800

    restyled shop

--- parts/product_block/index.php
+++ parts/product_block/index.php
@@ -22,23 +22,20 @@
 
 ?>
 
+<h3 class="<?php echo classes([$styles['title']]); ?>">
+	<a href="/product/<?php echo $product->get_slug(); ?>">
+		<?php echo $product->get_name(); ?>
+	</a>
+</h3>
 <div class="<?php echo classes([$styles['image']]); ?>">
 	<img  
 		src="<?php echo wp_get_attachment_image_src($image, 'event_feed_block')[0]; ?>" 
 		alt="<?php echo alt_text(get_post_thumbnail_id($image)); ?>" 
 	/>
 </div>
-<h3 class="<?php echo classes([$styles['title']]); ?>">
-	<a href="/product/<?php echo $product->get_slug(); ?>">
-		<?php echo $product->get_name(); ?>
-	</a>
-</h3>
 <p class="<?php echo classes([$styles['price']]); ?>">
 	<?php echo display_price(cfs_product_price($product)); ?>
 </p>
-<div class="<?php echo classes([$styles['excerpt']]); ?>">
-	<?php echo $product->excerpt; ?>
-</div>
 <a 
 	href="<?php echo $product->add_to_cart_url(); ?>"
 	class="<?php echo classes([$styles['cart']]); ?>"
